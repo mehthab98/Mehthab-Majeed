6708a198784e7536c75b2de457b26dbed3d08730
Mehthab <mehthabmajeed98@gmail.com>
2022-04-28 09:47:58 +0530

    StaticExample.java

---
+++ src/add2num.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class add2num {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int c=a+b;
+                System.out.println("The added value is : "+c);
+    }
+}
