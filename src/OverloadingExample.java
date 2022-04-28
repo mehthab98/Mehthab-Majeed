523e49bdaef4c78865c055cf9dabfaff71f1230f
Mehthab <mehthabmajeed98@gmail.com>
2022-04-28 11:54:48 +0530

    OverloadingExample.java

---
+++ src/OverloadingExample.java
@@ -0,0 +1,20 @@
+public class OverloadingExample {
+    static int add(int a,int b)
+
+    {
+        return a+b;
+    }
+static int add(int a, int b,int c)
+{
+    return a+b+c;
+}
+public class TestOverloading
+{
+    public static void main(String[] args)
+    {
+        System.out.println(OverloadingExample.add(11,11));
+        System.out.println(OverloadingExample.add(11,11,11));
+
+    }
+}
+}
